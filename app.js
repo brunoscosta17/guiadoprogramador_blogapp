a89bbf3244b7dd9b448bf24b5b7e1421f52ffcfe
= <=>
2019-05-03 00:15:33 -0300

    Configuring Sessions

--- app.js
+++ app.js
@@ -3,11 +3,26 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
-//const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const admin = require('./routes/admin');
 const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 // Configurations
+    // Session
+        app.use(session({
+            secret: "cursonode",
+            resave: true,
+            saveUninitialized: true
+        }));
+        app.use(flash());
+    // Middleware
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash('seccess_msg');
+        res.locals.error_msg = req.flash('error_msg');
+        next();
+    });
     // Body Parser
         app.use(bodyParser.urlencoded({ extended: true }));
         app.use(bodyParser.json());
@@ -17,9 +32,16 @@ const path = require('path');
         app.set('view engine', 'handlebars');
 
     // Mongoose
+        mongoose.Promise = global.Promise;
+        mongoose.connect("mongodb://localhost/blogapp")
+            .then(() => {
+                console.log("Conectado ao mongodb!");
+            }).catch( (error) => {
+                console.log("Erro ao se conectar ao mongo", error);
+            });
 
     // Public
-    app.use(express.static(path.join(__dirname,'public')));
+        app.use(express.static(path.join(__dirname,'public')));
 
 // Routes
 app.use('/admin', admin);
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+require('../models/Category');
+const Category = mongoose.model('category');
 
 router.get('/', (req, res) => {
     res.render('admin/index');
@@ -15,6 +18,20 @@ router.get('/categories', (req, res) => {
 
 router.get('/categories/add', (req, res) => {
     res.render('admin/addcategory');
+});
+
+router.post('/categories/new', (req, res) => {
+    const newCategory = {
+        name: req.body.name,
+        slug: req.body.slug
+    }
+
+    new Category(newCategory).save()
+        .then(() => {
+            console.log('Categoria salva com sucesso!')
+        }).catch((error) => {
+            console.log('Erro ao salvar categoria!')
+        });
 });
 
 module.exports = router;
