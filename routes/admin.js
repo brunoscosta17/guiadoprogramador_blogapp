c78a663e046f19ac5f0e28b90b6fb268c8f5d893
= <=>
2019-05-17 01:24:24 -0300

    Editing Categories

--- routes/admin.js
+++ routes/admin.js
@@ -13,9 +13,9 @@ router.get('/posts', (req, res) => {
 });
 
 router.get('/categories', (req, res) => {
-    Category.find().sort({createdAt: 'desc'})
+    Category.find().sort({ createdAt: 'desc' })
         .then((categories) => {
-            res.render('admin/categories', {categories: categories});
+            res.render('admin/categories', { categories: categories });
         }).catch((err) => {
             req.flash("error_msg", "Erro ao listar as categorias!");
             res.redirect('/admin');
@@ -30,24 +30,24 @@ router.post('/categories/new', (req, res) => {
 
     let errors = [];
 
-    if(!req.body.name || typeof req.body.name == undefined || req.body.name == null) {
-        errors.push({error: "Nome invalido!"})        
+    if (!req.body.name || typeof req.body.name == undefined || req.body.name == null) {
+        errors.push({ error: "Nome invalido!" })
     }
 
-    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
-        errors.push({error: "Slug invalido!"})
+    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        errors.push({ error: "Slug invalido!" })
     }
 
-    if(req.body.name.length < 3) {
-        errors.push({error: "O nome deve ter ao menos 3 caracteres!"})
+    if (req.body.name.length < 3) {
+        errors.push({ error: "O nome deve ter ao menos 3 caracteres!" })
     }
 
-    if(req.body.name.length < 3) {
-        errors.push({error: "O slug deve ter ao menos 3 caracteres!"})
+    if (req.body.name.length < 3) {
+        errors.push({ error: "O slug deve ter ao menos 3 caracteres!" })
     }
 
-    if(errors.length > 0) {
-        res.render("admin/addcategory", {errors: errors});
+    if (errors.length > 0) {
+        res.render("admin/addcategory", { errors: errors });
     } else {
         const newCategory = {
             name: req.body.name,
@@ -55,14 +55,67 @@ router.post('/categories/new', (req, res) => {
         }
 
         new Category(newCategory).save()
-        .then(() => {
-            req.flash("success_msg", "Categoria criada com suceso");
-            res.redirect('/admin/categories');
-        }).catch((error) => {
-            req.flash("error_msg", 'Erro ao salvar categoria!');
-            res.redirect('/admin');
-        });
+            .then(() => {
+                req.flash("success_msg", "Categoria criada com suceso");
+                res.redirect('/admin/categories');
+            }).catch((error) => {
+                req.flash("error_msg", 'Erro ao salvar categoria!');
+                res.redirect('/admin');
+            });
     }
+});
+
+router.get('/categories/edit/:id', (req, res) => {
+    Category.findOne({ _id: req.params.id })
+        .then((category) => {
+            res.render('admin/editcategory', { category: category });
+        }).catch((error) => {
+            req.flash("error_msg", "Categoria não encontrada!");
+            res.redirect("/admin/categories")
+        });
+});
+
+router.post('/categories/edit', (req, res) => {
+
+    let errors = [];
+
+    if (!req.body.name || typeof req.body.name === undefined || req.body.name == null) {
+        errors.push({ error: "Nome invalido!" })
+    }
+
+    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        errors.push({ error: "Slug invalido!" })
+    }
+
+    if (req.body.name.length < 3) {
+        errors.push({ error: "O nome deve ter ao menos 3 caracteres!" })
+    }
+
+    if (req.body.name.length < 3) {
+        errors.push({ error: "O slug deve ter ao menos 3 caracteres!" })
+    } if (errors.length > 0) {
+        res.render("admin/addcategory", { errors: errors });
+    } else {
+        Category.findOne({ _id: req.body.id })
+            .then((category) => {
+                category.name = req.body.name
+                category.slug = req.body.slug
+
+                    category.save()
+                        .then(() => {
+                            req.flash("success_msg", "Categoria editada com sucesso!");
+                            res.redirect("/admin/categories");
+                        }).catch((error) => {
+                            req.flash("error_msg", "Errro ao salvar a edicao da categoria!");
+                            res.redirect("/admin/categories");
+                        });
+            }).catch((error) => {
+                console.log(error);
+                req.flash("error_msg", "Erro ao editar a categoria!");
+                res.redirect("/admin/categories");
+            });
+    }
+
 });
 
 module.exports = router;
